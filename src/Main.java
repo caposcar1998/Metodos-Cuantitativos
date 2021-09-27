a3ef316a07bb7233ec4cbf7a97d40688b268116c
Akio Olvera <akiooarao@gmail.com>
2021-09-26 18:17:14 -0600

    Finishing chi squared

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        /*
         System.out.println("Centros cuadrados");
         CentrosCuadrados cc = new CentrosCuadrados(3708,7);
         cc.run();
@@ -17,7 +16,6 @@ public class Main {
         System.out.println("Generador Multiplicativo");
         GeneradorMultiplicativo gm = new GeneradorMultiplicativo(5,5,32);
         gm.run();
-        */
 
         Double[] testChi = {
                 8.223, 0.836, 2.634, 4.778, 0.406, 0.517, 2.330, 2.563, 0.511, 6.426,
@@ -28,7 +26,12 @@ public class Main {
         };
 
         List<Double> testChiList = Arrays.asList(testChi);
-        ChiCuadrada chi = new ChiCuadrada(testChiList);
-        chi.run();
+        ChiCuadrada chi = new ChiCuadrada(testChiList, 0.05);
+        boolean accepts = chi.run();
+        if (accepts) {
+            System.out.println("Chi cuadrada: Se acepta hipótesis nula");
+        } else {
+            System.out.println("Chi cuadrada: Se rechaza hipótesis nula");
+        }
     }
 }
